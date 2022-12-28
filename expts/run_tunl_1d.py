efde15050ac54c1252844ba2b3c79294fc9fd288
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2022-12-27 23:35:39 -0500

    load models and continue tunl1d int4 training

--- expts/run_tunl_1d.py
+++ expts/run_tunl_1d.py
@@ -55,7 +55,7 @@ hidden_type = argsdict['hidden_type']
 if record_data:
     main_dir = '/network/scratch/l/lindongy/timecell/data_collecting/tunl1d'
 else:
-    main_dir = '/network/scratch/l/lindongy/timecell/training/tunl1d_gamma1'
+    main_dir = '/network/scratch/l/lindongy/timecell/training/tunl1d_inp4'
 save_dir = os.path.join(main_dir, f'{env_type}_{len_delay}_{hidden_type}_{n_neurons}_{lr}')
 if not os.path.exists(save_dir):
     os.mkdir(save_dir)
@@ -83,7 +83,7 @@ else:
     # assert loaded model has congruent hidden type and n_neurons
     assert hidden_type in ckpt_name, 'Must load network with the same hidden type'
     assert str(n_neurons) in ckpt_name, 'Must load network with the same number of hidden neurons'
-    net.load_state_dict(torch.load(os.path.join('/network/scratch/l/lindongy/timecell/training/tunl1d', load_model_path), map_location=torch.device('cpu')))
+    net.load_state_dict(torch.load(os.path.join('/network/scratch/l/lindongy/timecell/training/tunl1d_inp4', load_model_path), map_location=torch.device('cpu')))
 
 stim = np.zeros(n_total_episodes, dtype=np.int8)  # 0=L, 1=R
 choice = np.zeros(n_total_episodes, dtype=np.int8)  # 0=L, 1=R
@@ -122,7 +122,7 @@ for i_episode in tqdm(range(n_total_episodes)):  # one episode = one sample
         correct_perc[i_episode] = 1
     if record_data:
         delay_resp[i_episode][:len(resp)] = np.asarray(resp)
-    p_loss, v_loss = finish_trial(net, 1, optimizer)
+    p_loss, v_loss = finish_trial(net, 0.9999, optimizer)
     if (i_episode+1) % save_ckpt_per_episodes == 0:
         print(f'Episode {i_episode}, {np.mean(correct_perc[i_episode+1-save_ckpt_per_episodes:i_episode+1])*100:.3f}% correct in the last {save_ckpt_per_episodes} episodes, avg {np.mean(correct_perc[:i_episode+1])*100:.3f}% correct')
         if save_ckpts:
