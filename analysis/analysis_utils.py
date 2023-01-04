b94189ba861789c59c2e576f12b1bd4e4839ef6e
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2023-01-03 22:28:59 -0500

    added make_venn_diagram

--- analysis/analysis_utils.py
+++ analysis/analysis_utils.py
@@ -7,6 +7,7 @@ import torch
 import numpy as np
 from analysis.linclab_utils import plot_utils
 import os
+from matplotlib_venn import venn2
 
 
 def bin_rewards(epi_rewards, window_size):
@@ -43,6 +44,18 @@ def make_piechart(n_ramp_neurons, n_seq_neurons, n_neurons, save_dir, label, sav
     else:
         plt.show()
 
+
+def make_venn_diagram(cell_nums_ramp, cell_nums_seq, n_neurons, save_dir, label, save=False):
+    total_neuron_idx = np.arange(n_neurons)
+    venn2(subsets=(len(cell_nums_ramp), len(cell_nums_seq), len(np.intersect1d(cell_nums_ramp, cell_nums_seq))), \
+          set_labels=('Ramping', 'Sequence'))
+    plt.gca().set_facecolor('skyblue')
+    plt.gca().set_axis_on()
+    plt.title(label)
+    if save:
+        plt.savefig(os.path.join(save_dir, 'venndiagram.svg'))
+    else:
+        plt.show()
 
 def plot_sorted_averaged_resp(cell_nums, sorted_matrix, title, remove_nan=True, save_dir=None, save=False):
     """
@@ -368,41 +381,6 @@ def time_decode(delay_resp, len_delay, n_neurons, bin_size, save_dir, title, plo
     return p_matrix, time_decode_error, time_decode_entropy
 
 
-def sort_resp(total_resp, norm=True):
-    """
-    Average the responses across episodes, normalize the activity according to the
-    maximum and minimum of each cell (optional), and sort cells by when their maximum response happens.
-    returns: cell_nums, sorted_matrix
-    """
-    np.seterr(divide='ignore', invalid='ignore')
-    n_neurons = np.shape(total_resp)[2]
-    segments = np.moveaxis(total_resp, 0, 1)
-    unsorted_matrix = np.zeros((n_neurons, len(segments)))  # len(segments) is also len_delay
-    sorted_matrix = np.zeros((n_neurons, len(segments)))
-    for i in range(len(segments)):  # at timestep i
-        averages = np.mean(segments[i],
-                           axis=0)  # 1 x n_neurons, each entry is the average response of this neuron at this time step across episodes
-        unsorted_matrix[:, i] = np.transpose(
-            averages)  # goes into the i-th column of unsorted_matrix, each row is one neuron
-        if norm is True:
-            normalized_matrix = (unsorted_matrix - np.min(unsorted_matrix, axis=1, keepdims=True)) / np.ptp(
-                unsorted_matrix, axis=1, keepdims=True)
-            # 0=minimum response of this neuron over time, 1=maximum response of this neuro over time
-            max_indeces = np.argmax(normalized_matrix, axis=1)  # which time step does the maximum firing occur
-            cell_nums = np.argsort(max_indeces)  # returns the order of cell number that should go into sorted_matrix
-            for i, i_cell in enumerate(list(cell_nums)):
-                sorted_matrix[i] = normalized_matrix[i_cell]
-        else:
-            max_indeces = np.argmax(unsorted_matrix, axis=1)  # which time step does the maximum firing occur
-            cell_nums = np.argsort(max_indeces)  # returns the order of cell number that should go into sorted_matrix
-            for i, i_cell in enumerate(list(cell_nums)):
-                sorted_matrix[i] = unsorted_matrix[i_cell]
-    # At this point, sorted_matrix should contain all cells
-    assert len(sorted_matrix) == n_neurons
-
-    return cell_nums, sorted_matrix
-
-
 def plot_half_split_resp(total_resp, save_dir, split='random', save=False):
     n_total_episodes = np.shape(total_resp)[0]
     len_delay = np.shape(total_resp)[1]
@@ -600,3 +578,38 @@ def split_train_and_test(percent_train, total_resp, total_stim, seed):
     stimuli_train = total_stim[itrain]
     resp_train = total_resp[itrain]
     return resp_train, resp_test, stimuli_train, stimuli_test
+
+
+def sort_resp(total_resp, norm=True):
+    """
+    Average the responses across episodes, normalize the activity according to the
+    maximum and minimum of each cell (optional), and sort cells by when their maximum response happens.
+    returns: cell_nums, sorted_matrix
+    """
+    np.seterr(divide='ignore', invalid='ignore')
+    n_neurons = np.shape(total_resp)[2]
+    segments = np.moveaxis(total_resp, 0, 1)
+    unsorted_matrix = np.zeros((n_neurons, len(segments)))  # len(segments) is also len_delay
+    sorted_matrix = np.zeros((n_neurons, len(segments)))
+    for i in range(len(segments)):  # at timestep i
+        averages = np.mean(segments[i],
+                           axis=0)  # 1 x n_neurons, each entry is the average response of this neuron at this time step across episodes
+        unsorted_matrix[:, i] = np.transpose(
+            averages)  # goes into the i-th column of unsorted_matrix, each row is one neuron
+        if norm is True:
+            normalized_matrix = (unsorted_matrix - np.min(unsorted_matrix, axis=1, keepdims=True)) / np.ptp(
+                unsorted_matrix, axis=1, keepdims=True)
+            # 0=minimum response of this neuron over time, 1=maximum response of this neuro over time
+            max_indeces = np.argmax(normalized_matrix, axis=1)  # which time step does the maximum firing occur
+            cell_nums = np.argsort(max_indeces)  # returns the order of cell number that should go into sorted_matrix
+            for i, i_cell in enumerate(list(cell_nums)):
+                sorted_matrix[i] = normalized_matrix[i_cell]
+        else:
+            max_indeces = np.argmax(unsorted_matrix, axis=1)  # which time step does the maximum firing occur
+            cell_nums = np.argsort(max_indeces)  # returns the order of cell number that should go into sorted_matrix
+            for i, i_cell in enumerate(list(cell_nums)):
+                sorted_matrix[i] = unsorted_matrix[i_cell]
+    # At this point, sorted_matrix should contain all cells
+    assert len(sorted_matrix) == n_neurons
+
+    return cell_nums, sorted_matrix
