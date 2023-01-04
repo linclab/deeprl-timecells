4a84ab599c8c58b95a72f3f2f4f66c872b8d51b5
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2023-01-03 23:34:24 -0500

    select ramping and sequence cell based on stats tests

--- analysis/analysis_script_int_discrim.py
+++ analysis/analysis_script_int_discrim.py
@@ -1,5 +1,5 @@
 from utils_int_discrim import *
-from analysis_utils import single_cell_visualization, time_decode, time_decode_lin_reg
+from analysis_utils import single_cell_visualization, time_decode, time_decode_lin_reg, make_venn_diagram
 from analysis.linclab_utils import plot_utils
 import numpy as np
 import os
@@ -80,7 +80,6 @@ last_step_resp = stim2_resp[np.arange(n_total_episodes), stim[:,1]-1, :]
 
 # Plot cell activities in all combinations of stimulus length
 
-plot_time_cell(stim[:,0], stim1_resp, label="First_Stim_", save_dir=save_dir, save=False)  # Plots time cell sequences and pie charts
 plot_time_cell_sorted_same_order(stim, stim1_resp, stim2_resp,save_dir=save_dir, save=False)  # Plot stim1 and stim2 time cells sorted in same order
 single_cell_temporal_tuning(stim, stim1_resp, stim2_resp, save_dir=save_dir, compare_correct=False)  # if compare_correct, then SAVE AUTOMATICALLY
 retiming(stim, stim1_resp, stim2_resp, save_dir=save_dir, verbose=True)  # SAVE AUTOMATICALLY. Takes ~10 minutes
@@ -95,10 +94,69 @@ compare_correct_vs_incorrect(stim1_resp, stim, correct_trials, analysis="decodin
 compare_correct_vs_incorrect(stim1_resp, stim, correct_trials, analysis="population", resp2=stim2_resp, title='', save_dir=save_dir, save=False)  # plot population sequence for corr and incorr trials
 # single_cell_visualization(stim2_resp, correct_trials, np.arange(n_neurons), "")
 
-# analyze delay period activity
-cell_nums_all, sorted_matrix_all, cell_nums_seq, sorted_matrix_seq, cell_nums_ramp, sorted_matrix_ramp = separate_ramp_and_seq(
-delay_resp, norm=True)
-plot_sorted_averaged_resp(cell_nums_all, sorted_matrix_all, title="delay_phase_population_activity", remove_nan=True, save_dir=save_dir, save=False)
+
+# =============================================
+
+# Identify time cells and ramping cells
+# =============================================
+
+# Define period that we want to analyse
+resp = stim1_resp  # Or stim2_resp or delay_resp
+stimulus = stim[:, 0]  # or stim[:, 1]
+label = 'stimulus_1'
+# Identifying ramping cells
+p_result, slope_result, intercept_result, R_result = lin_reg_ramping(resp)
+ramp_cell_bool = np.logical_and(p_result<=0.05, slope_result<=0.05)
+cell_nums_ramp = np.where(ramp_cell_bool)[0]
+
+# Identifying sequence cells
+RB_result, z_RB_threshold_result = ridge_to_background(resp, ramp_cell_bool, percentile=95, n_shuff=1000)
+seq_cell_bool = RB_result > z_RB_threshold_result
+cell_nums_seq = np.where(seq_cell_bool)[0]
+
+if np.shape(resp)[1] == 20:  # Delay period with consistent trial duration
+    I_result, I_threshold_result = skaggs_temporal_information(resp, n_shuff=1000, percentile=95)
+else:
+    I_result, I_threshold_result = skaggs_temporal_information_varying_duration(resp, stim, n_shuff=1000, percentile=95)
+high_temporal_info_cell_bool = I_result > I_threshold_result
+
+R_result, pval_result = trial_consistency_across_durations(stim, stim1_resp, stim2_resp, type='absolute')  # also try 'relative'
+
+cell_nums_nontime = np.remove(np.arange(n_neurons), np.intersect1d(cell_nums_seq, cell_nums_ramp))
+
+
+# Here, the cell_nums have not been re-arranged according to peak latency yet
+resp_ramp = resp[:, :, cell_nums_ramp]
+resp_seq = resp[:, :, cell_nums_seq]
+resp_nontime = resp[:, :, cell_nums_nontime]
+n_ramp_neurons = len(cell_nums_ramp)
+n_seq_neurons = len(cell_nums_seq)
+n_nontime_neurons = len(cell_nums_nontime)
+
+
+# See if time cell/ramping cell/nontime cells can decode time
+time_decode_all_stim_len(resp[cell_nums_ramp], stimulus, title=label+' ramp', save_dir=save_dir, save=False)  # decode time, grouped by stimulus length
+time_decode_all_stim_len(resp[cell_nums_seq], stimulus, title=label+' seq', save_dir=save_dir, save=False)
+time_decode_all_stim_len(resp[cell_nums_nontime], stimulus, title=label+' nontime', save_dir=save_dir, save=False)
+time_decode_lin_reg(resp[cell_nums_ramp], np.shape(resp[1]), n_ramp_neurons, 1000, title=label+' ramp', save_dir=save_dir, save=False)
+time_decode_lin_reg(resp[cell_nums_seq], np.shape(resp[1]), n_seq_neurons, 1000, title=label+' seq', save_dir=save_dir, save=False)
+time_decode_lin_reg(resp[cell_nums_nontime], np.shape(resp[1]), n_nontime_neurons, 1000, title=label+' nontime', save_dir=save_dir, save=False)
+
+
+# Re-arrange cell_nums according to peak latency
+cell_nums_all, cell_nums_seq, cell_nums_ramp, cell_nums_nontime, \
+sorted_matrix_all, sorted_matrix_seq, sorted_matrix_ramp, sorted_matrix_nontime = \
+    sort_response_by_peak_latency(resp, cell_nums_ramp, cell_nums_seq, norm=True)
+
+# print('Make a venn diagram of neuron counts...')
+make_venn_diagram(cell_nums_ramp, cell_nums_seq, n_neurons, title=label, save_dir=save_dir, save=False)
+
+
+plot_sorted_averaged_resp(cell_nums_all, sorted_matrix_all, title=label+"_all_cell_activity", remove_nan=True, save_dir=save_dir, save=False)
+plot_sorted_averaged_resp(cell_nums_seq, sorted_matrix_seq, title=label+"_seq_cell_activity", remove_nan=True, save_dir=save_dir, save=False)
+plot_sorted_averaged_resp(cell_nums_ramp, sorted_matrix_ramp, title=label+"_ramp_cell_activity", remove_nan=True, save_dir=save_dir, save=False)
+plot_sorted_averaged_resp(cell_nums_nontime, sorted_matrix_nontime, title=label+"_nontime_cell_activity", remove_nan=True, save_dir=save_dir, save=False)
+
 
 # ------------------------------ Lesion experiment ----------------------------------
 
