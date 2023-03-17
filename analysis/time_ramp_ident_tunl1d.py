fixed normalization axis

analysis/time_ramp_ident_tunl1d.py
@@ -56,7 +56,7 @@ n_total_episodes = np.shape(stim)[0]
 normalize = True if argsdict['normalize'] == True or argsdict['normalize'] == 'True' else False
 if normalize:
     reshape_resp = np.reshape(delay_resp, (n_total_episodes*len_delay, n_neurons))
-    reshape_resp = (reshape_resp - np.min(reshape_resp, axis=1, keepdims=True)) / np.ptp(reshape_resp, axis=1, keepdims=True)
+    reshape_resp = (reshape_resp - np.min(reshape_resp, axis=0, keepdims=True)) / np.ptp(reshape_resp, axis=0, keepdims=True)
     delay_resp = np.reshape(reshape_resp, (n_total_episodes, len_delay, n_neurons))
 
 