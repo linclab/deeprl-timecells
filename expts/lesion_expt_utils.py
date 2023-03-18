a14626bc4e3959d9dbc8d732c5588b94a4713879
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2023-03-18 17:16:59 -0400

    change name from seq to time

--- expts/lesion_expt_utils.py
+++ expts/lesion_expt_utils.py
@@ -6,7 +6,7 @@ def generate_random_index(num_shuffle, n_neurons, cell_nums_ramp, cell_nums_seq)
     random_index_dict = {
         'random': np.zeros((num_shuffle, n_neurons)),
         'ramp': np.zeros((num_shuffle, n_neurons)),
-        'seq': np.zeros((num_shuffle, n_neurons))
+        'time': np.zeros((num_shuffle, n_neurons))
     }
 
     for i_shuffle in range(num_shuffle):
@@ -24,6 +24,6 @@ def generate_random_index(num_shuffle, n_neurons, cell_nums_ramp, cell_nums_seq)
         non_seq_nums = np.setdiff1d(np.arange(n_neurons), cell_nums_seq)
         np.random.shuffle(non_seq_nums)
         seq_index = np.concatenate((cell_nums_seq_shuffled, non_seq_nums))
-        random_index_dict['seq'][i_shuffle] = seq_index
+        random_index_dict['time'][i_shuffle] = seq_index
 
     return random_index_dict
