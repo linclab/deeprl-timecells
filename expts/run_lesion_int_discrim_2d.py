f9daa4c5876437f4f50308dfdd9f1b331c6c7574
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2023-03-19 09:56:51 -0400

    allow save_net_and_data path to exist

--- expts/run_lesion_int_discrim_2d.py
+++ expts/run_lesion_int_discrim_2d.py
@@ -88,7 +88,8 @@ def lesion_experiment(env, net, optimizer, n_total_episodes, lesion_idx, title, 
 
     if save_net_and_data:
         net_and_data_dir = os.path.join(save_dir, f'epi{n_total_episodes}_shuff{num_shuffle}_idx{lesion_idx_start}_{lesion_idx_step}_{n_neurons if lesion_idx_end is None else lesion_idx_end}_net_and_data')
-        os.mkdir(net_and_data_dir)
+        if not os.path.exists(net_and_data_dir):
+            os.mkdir(net_and_data_dir)
         #torch.save(net.state_dict(), os.path.join(net_and_data_dir, f'postlesion_{title}.pt'))
         np.savez_compressed(os.path.join(net_and_data_dir, f'lesion_{title}_data.npz'), reward_hist=reward_hist, correct_perc=correct_perc,
                             stim=stim, stim1_resp_hx=stim_1_resp,
