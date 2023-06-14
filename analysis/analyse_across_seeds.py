bd55b1ef211ba0c0ae50b91e88c5da30b36fa0e2
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2023-06-14 18:09:26 -0400

    plot every 10 seeds

--- analysis/analyse_across_seeds.py
+++ analysis/analyse_across_seeds.py
@@ -11,9 +11,54 @@ from analysis.utils_mutual_info import joint_encoding_information_time_stimulus
 from analysis import utils_linclab_plot
 utils_linclab_plot.linclab_plt_defaults(font="Arial", fontdir="analysis/fonts")
 
+
+def plot_time_decoding_across_seeds(t_test_dict, t_test_pred_dict, len_delay):
+    print("Plot time decoding across seeds...")
+    t_test_arr = np.array(list(t_test_dict.values())).flatten()
+    t_test_pred_arr = np.array(list(t_test_pred_dict.values())).flatten()
+    mean_pred = np.zeros(len_delay)
+    std_pred = np.zeros(len_delay)
+    for t_elapsed in range(len_delay):
+        mean_pred[t_elapsed] = np.mean(t_test_pred_arr[t_test_arr == t_elapsed])
+        std_pred[t_elapsed] = np.std(t_test_pred_arr[t_test_arr == t_elapsed])# 1 x len_delay
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax.scatter(x=t_test_arr, y=t_test_pred_arr,s=1)
+    ax.set_xlabel('Time since delay onset')
+    ax.set_ylabel('Decoded time')
+    ax.plot(np.arange(len_delay), np.arange(len_delay),color='k')
+    ax.plot(np.arange(len_delay), mean_pred, color=utils_linclab_plot.LINCLAB_COLS['blue'])
+    ax.fill_between(np.arange(len_delay), mean_pred-std_pred,  mean_pred+std_pred, color='skyblue',alpha=0.4)
+    ax.set_xticks([0, len_delay])
+    ax.set_xticklabels(['0', str(len_delay)])
+    ax.set_yticks([0, len_delay])
+    ax.set_yticklabels(['0', str(len_delay)])
+    ax.set_title(f'Time decoding pooled across {len(t_test_dict)} seeds')
+    ax.set_xlim((np.min(t_test_pred_arr),41))
+    ax.set_ylim((np.min(t_test_pred_arr),np.max(t_test_pred_arr)))
+    fig.savefig(os.path.join(save_dir, f'time_decoding_pooled_{len(t_test_dict)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.svg'), dpi=300)
+
+
+def plot_mutual_information_across_seeds(info_dict):
+    print("Plot mutual information across seeds...")
+    info_arr = np.vstack(info_dict.values())
+    n_total_neurons = np.shape(info_arr)[0]
+    breakpoint()
+    stats = cbook.boxplot_stats(info_arr, labels=['Stim x Time', r'$Stim x Rand(Time)$', r'$Time x Rand(Stim)$'], bootstrap=10000)
+    for i in range(len(stats)):
+        stats[i]['whislo'] = np.min(info_arr[:,i], axis=0)
+        stats[i]['whishi'] = np.max(info_arr[:,i], axis=0)
+    fig, axs = plt.subplots(1,1)
+    fig.suptitle(f'Mutual Information pooled across {len(info_dict)} seeds')
+    for i in range(n_total_neurons):
+        plt.plot([1, 2, 3], info_arr[i,:], color="gray", lw=1)
+    props = dict(color='indigo', linewidth=1.5)
+    axs.bxp(stats, showfliers=False, boxprops=props,
+            capprops=props, whiskerprops=props, medianprops=props)
+    plt.ylabel("Mutual information (bits)", fontsize=19)
+    plt.savefig(os.path.join(save_dir, f'joint_encoding_info_pooled_{len(info_dict)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.svg'))
+
+
 data_dir = '/network/scratch/l/lindongy/timecell/data_collecting/tunl1d_og/mem_40_lstm_128_0.0001'
-# find all .npy files that match the name f'mem_40_lstm_128_0.0001_seed_{seed}_epi199999.pt_data.npz
-# and extract the seed number
 seed_list = []
 for file in os.listdir(data_dir):
     if re.match(f'mem_40_lstm_128_0.0001_seed_\d+_epi199999.pt_data.npz', file):
@@ -29,18 +74,19 @@ seed_list = seed_list[:2]
 
 save_dir = '/network/scratch/l/lindongy/timecell/figures/fig_2/tunl1d'
 os.makedirs(save_dir, exist_ok=True)
-load= True
+load = True
+
 if load:
-    t_test_arr = np.load(os.path.join(save_dir, f't_test_arr_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), allow_pickle=True).item()
-    t_test_pred_arr = np.load(os.path.join(save_dir, f't_test_pred_arr_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), allow_pickle=True).item()
-    info_arr = np.load(os.path.join(save_dir, f'info_arr_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), allow_pickle=True).item()
+    t_test_dict = np.load(os.path.join(save_dir, f't_test_dict_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), allow_pickle=True).item()
+    t_test_pred_dict = np.load(os.path.join(save_dir, f't_test_pred_dict_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), allow_pickle=True).item()
+    info_dict = np.load(os.path.join(save_dir, f'info_dict_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), allow_pickle=True).item()
 else:
     ramping_cell_ids = {}
     time_cell_ids = {}
-    t_test_arr = {}
-    t_test_pred_arr = {}
-    info_arr = {}  # n_seeds x 128 x 3
-    for each_seed in seed_list:
+    t_test_dict = {}
+    t_test_pred_dict = {}
+    info_dict = {}
+    for i_seed, each_seed in enumerate(seed_list):
         print(f'====================== Analyzing seed {each_seed} ...======================================')
         seed_save_dir = os.path.join(save_dir, f'seed_{each_seed}')
         if not os.path.exists(seed_save_dir):
@@ -107,68 +153,25 @@ else:
 
         # Decode time
         print("Decode time...")
-        t_test, t_test_pred = time_decode_lin_reg(delay_resp, len_delay, n_neurons, 1000, title='all_cells', save_dir=seed_save_dir, save=True)
-        t_test_arr[each_seed] = t_test
-        t_test_pred_arr[each_seed] = t_test_pred
+        t_test_dict[each_seed], t_test_pred_dict[each_seed] = time_decode_lin_reg(delay_resp, len_delay, n_neurons, 1000, title='all_cells', save_dir=seed_save_dir, save=True)
 
         # Mutual Information
         print("Mutual Information...")
-        info = joint_encoding_information_time_stimulus(delay_resp, stim, save_dir=seed_save_dir,title=f'{each_seed}_{n_total_episodes}_{n_shuffle}_{percentile}', logInfo=False, save=True)
-        info_arr[each_seed] = info
+        info_dict[each_seed] = joint_encoding_information_time_stimulus(delay_resp, stim, save_dir=seed_save_dir,title=f'{each_seed}_{n_total_episodes}_{n_shuffle}_{percentile}', logInfo=False, save=True)
 
+        # for each 10 seeds, plot the results
+        if (i_seed+1) % 10 == 0:
+            # plot time decoding
+            plot_time_decoding_across_seeds(t_test_dict, t_test_pred_dict, len_delay)
+            # plot mutual information
+            plot_mutual_information_across_seeds(info_dict)
     # Save results
     print("Save results..")
-    np.save(os.path.join(save_dir, f't_test_arr_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), t_test_arr)
-    np.save(os.path.join(save_dir, f't_test_pred_arr_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), t_test_pred_arr)
-    np.save(os.path.join(save_dir, f'info_arr_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), info_arr)
+    np.save(os.path.join(save_dir, f't_test_dict_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), t_test_dict)
+    np.save(os.path.join(save_dir, f't_test_pred_dict_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), t_test_pred_dict)
+    np.save(os.path.join(save_dir, f'info_dict_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), info_dict)
     np.save(os.path.join(save_dir, f'time_cell_ids_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), time_cell_ids)
     np.save(os.path.join(save_dir, f'ramping_cell_ids_{n_total_episodes}_{n_shuffle}_{percentile}.npy'), ramping_cell_ids)
-
-# Plot results
-# Plot time decoding across seeds
-print("Plot time decoding across seeds...")
-t_test_arr = np.array(list(t_test_arr.values())).flatten()
-t_test_pred_arr = np.array(list(t_test_pred_arr.values())).flatten()
-mean_pred = np.zeros(len_delay)
-std_pred = np.zeros(len_delay)
-for t_elapsed in range(len_delay):
-    mean_pred[t_elapsed] = np.mean(t_test_pred_arr[t_test_arr == t_elapsed])
-    std_pred[t_elapsed] = np.std(t_test_pred_arr[t_test_arr == t_elapsed])# 1 x len_delay
-fig, ax = plt.subplots(figsize=(10,10))
-ax.scatter(x=t_test_arr, y=t_test_pred_arr,s=1)
-ax.set_xlabel('Time since delay onset')
-ax.set_ylabel('Decoded time')
-ax.plot(np.arange(len_delay), np.arange(len_delay),color='k')
-ax.plot(np.arange(len_delay), mean_pred, color=utils_linclab_plot.LINCLAB_COLS['blue'])
-ax.fill_between(np.arange(len_delay), mean_pred-std_pred,  mean_pred+std_pred, color='skyblue',alpha=0.4)
-ax.set_xticks([0, len_delay])
-ax.set_xticklabels(['0', str(len_delay)])
-ax.set_yticks([0, len_delay])
-ax.set_yticklabels(['0', str(len_delay)])
-ax.set_title(f'Time decoding pooled across {len(seed_list)} seeds')
-ax.set_xlim((np.min(t_test_pred_arr),41))
-ax.set_ylim((np.min(t_test_pred_arr),np.max(t_test_pred_arr)))
-fig.savefig(os.path.join(save_dir, f'time_decoding_pooled_{n_total_episodes}_{n_shuffle}_{percentile}.svg'), dpi=300)
-
-# Plot mutual information across seeds
-print("Plot mutual information across seeds...")
-info_arr = np.vstack(info_arr.values())
-n_total_neurons = np.shape(info_arr)[0]
-breakpoint()
-stats = cbook.boxplot_stats(info_arr, labels=['Stim x Time', r'$Stim x Rand(Time)$', r'$Time x Rand(Stim)$'], bootstrap=10000)
-for i in range(len(stats)):
-    stats[i]['whislo'] = np.min(info_arr[:,i], axis=0)
-    stats[i]['whishi'] = np.max(info_arr[:,i], axis=0)
-fig, axs = plt.subplots(1,1)
-fig.suptitle('Mutual Information pooled across seeds')
-for i in range(n_total_neurons):
-    plt.plot([1, 2, 3], info_arr[i,:], color="gray", lw=1)
-props = dict(color='indigo', linewidth=1.5)
-axs.bxp(stats, showfliers=False, boxprops=props,
-        capprops=props, whiskerprops=props, medianprops=props)
-plt.ylabel("Mutual information (bits)", fontsize=19)
-plt.savefig(os.path.join(save_dir, f'joint_encoding_info_pooled_{n_total_episodes}_{n_shuffle}_{percentile}.svg'))
-
 
 
 
