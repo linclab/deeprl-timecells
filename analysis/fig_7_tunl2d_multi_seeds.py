6b6af479f88e5220e54f0695211da7d81fbcfc27
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2023-06-25 12:29:20 -0400

    tunl2d multiseed

--- analysis/fig_7_tunl2d_multi_seeds.py
+++ analysis/fig_7_tunl2d_multi_seeds.py
@@ -1,6 +1,7 @@
 import os
 import re
 import numpy as np
+import argparse
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 import sys
@@ -12,17 +13,293 @@ from analysis.utils_mutual_info import joint_encoding_information_time_stimulus
 from analysis import utils_linclab_plot
 utils_linclab_plot.linclab_plt_defaults(font="Arial", fontdir="analysis/fonts")
 
+def plot_time_decoding_across_seeds(t_test_dict, t_test_pred_dict, len_delay):
+    print("Plot time decoding across seeds...")
+    t_test_arr = np.array(list(t_test_dict.values())).flatten()
+    t_test_pred_arr = np.array(list(t_test_pred_dict.values())).flatten()
+    mean_pred = np.zeros(len_delay)
+    std_pred = np.zeros(len_delay)
+    for t_elapsed in range(len_delay):
+        mean_pred[t_elapsed] = np.mean(t_test_pred_arr[t_test_arr == t_elapsed])
+        std_pred[t_elapsed] = np.std(t_test_pred_arr[t_test_arr == t_elapsed])# 1 x len_delay
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax.scatter(x=t_test_arr, y=t_test_pred_arr,s=1)
+    ax.set_xlabel('Time since delay onset')
+    ax.set_ylabel('Decoded time')
+    ax.plot(np.arange(len_delay), np.arange(len_delay),color='k')
+    ax.plot(np.arange(len_delay), mean_pred, color=utils_linclab_plot.LINCLAB_COLS['blue'])
+    ax.fill_between(np.arange(len_delay), mean_pred-std_pred,  mean_pred+std_pred, color='skyblue',alpha=0.4)
+    ax.set_xticks([0, len_delay])
+    ax.set_xticklabels(['0', str(len_delay)])
+    ax.set_yticks([0, len_delay])
+    ax.set_yticklabels(['0', str(len_delay)])
+    ax.set_title(f'Time decoding pooled across {len(t_test_dict)} seeds')
+    ax.set_xlim((np.min(t_test_pred_arr),41))
+    ax.set_ylim((np.min(t_test_pred_arr),np.max(t_test_pred_arr)))
+    fig.savefig(os.path.join(save_dir, f'time_decoding_pooled_{len(t_test_dict)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.svg'), dpi=300)
+    fig.savefig(os.path.join(save_dir, f'time_decoding_pooled_{len(t_test_dict)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.png'), dpi=300)
 
-import argparse
+
+def plot_ST_mutual_information_across_seeds(info_dict):
+    print("Plot mutual information across seeds...")
+    info_arr = np.vstack(info_dict.values())
+    n_total_neurons = np.shape(info_arr)[0]
+    stats = cbook.boxplot_stats(info_arr, labels=['Stim x Time', r'$Stim x Rand(Time)$', r'$Time x Rand(Stim)$'], bootstrap=10000)
+    for i in range(len(stats)):
+        stats[i]['whislo'] = np.min(info_arr[:,i], axis=0)
+        stats[i]['whishi'] = np.max(info_arr[:,i], axis=0)
+    fig, axs = plt.subplots(1,1)
+    fig.suptitle(f'Mutual Information pooled across {len(info_dict)} seeds')
+    # for i in range(n_total_neurons):
+    #     plt.plot([1, 2, 3], info_arr[i,:], color="gray", lw=1)
+    props = dict(color='indigo', linewidth=1.5)
+    axs.bxp(stats, showfliers=False, boxprops=props,
+            capprops=props, whiskerprops=props, medianprops=props)
+    plt.ylabel("Mutual information (bits)", fontsize=19)
+    #Run nonparametric test on unrmd, stimrmd, timermd pairwise, and print p-values
+    print(f"Pooled_{len(info_dict)}seeds: Kruskal-Wallis test p-values:")
+    unrmd, timermd, stimrmd = info_arr[:,0], info_arr[:,1], info_arr[:,2]
+    print("Unrmd vs. Stimrmd: ", kruskal(unrmd, stimrmd)[1])
+    print("Unrmd vs. Timermd: ", kruskal(unrmd, timermd)[1])
+    print("Stimrmd vs. Timermd: ", kruskal(stimrmd, timermd)[1])
+    plt.savefig(os.path.join(save_dir, f'joint_encoding_info_noloine_pooled_{len(info_dict)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.svg'))
+    plt.savefig(os.path.join(save_dir, f'joint_encoding_info_noline_pooled_{len(info_dict)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.png'))
+
+
+def plot_count_time_and_ramping_cells(time_cell_ids, ramping_cell_ids):
+    time_cell_counts = []
+    ramping_cell_counts = []
+    both_cell_counts = []
+    neither_cell_counts = []
+    for each_seed in time_cell_ids.keys():
+        time_cell_counts.append(len(time_cell_ids[each_seed]['total']))
+        ramping_cell_counts.append(len(ramping_cell_ids[each_seed]['total']))
+        both_cell_counts.append(len(np.intersect1d(time_cell_ids[each_seed]['total'], ramping_cell_ids[each_seed]['total'])))
+        not_time_cell = np.setdiff1d(np.arange(n_neurons), time_cell_ids[each_seed]['total'])
+        not_ramping_cell = np.setdiff1d(np.arange(n_neurons), ramping_cell_ids[each_seed]['total'])
+        neither_cell_counts.append(len(np.intersect1d(not_time_cell, not_ramping_cell)))
+    # convert to numpy array
+    time_cell_counts = np.array(time_cell_counts)
+    ramping_cell_counts = np.array(ramping_cell_counts)
+    both_cell_counts = np.array(both_cell_counts)
+    neither_cell_counts = np.array(neither_cell_counts)
+    # plot as bar plot with error bar
+    fig, ax = plt.subplots()
+    ax.bar(x=np.arange(4), height=[np.mean(time_cell_counts), np.mean(ramping_cell_counts), np.mean(both_cell_counts), np.mean(neither_cell_counts)], yerr=[np.std(time_cell_counts), np.std(ramping_cell_counts), np.std(both_cell_counts), np.std(neither_cell_counts)])
+    ax.set_xticks(np.arange(4))
+    ax.set_xticklabels(['Time cells', 'Ramping cells', 'Both', 'Neither'])
+    ax.set_title(f'Cell type count across {len(time_cell_ids)} seeds')
+    ax.axhline(y=n_neurons, linestyle='--', color='k')
+    plt.savefig(os.path.join(save_dir, f'cell_type_count_{len(time_cell_ids)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.svg'))
+    plt.savefig(os.path.join(save_dir, f'cell_type_count_{len(time_cell_ids)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.png'))
+
+
+def plot_r_across_seeds(r_dict):
+    print("Plot r across seeds...")
+    r_list = []
+    for each_seed in r_dict.keys():
+        r_list.append(r_dict[each_seed])
+    r_list = np.concatenate(r_list)
+    plt.figure()
+    plt.hist(r_list, range=(-1,1), bins=50)
+    plt.xlabel(f"r(left, right)")
+    plt.ylabel("Fraction")
+    plt.savefig(os.path.join(save_dir, f"left_right_r_hist.svg"))
+    plt.savefig(os.path.join(save_dir, f"left_right_r_hist.png"))
+
+
+def plot_stim_decoding_accuracy_across_seeds(accuracies_dict, accuracies_shuff_dict, pick_one=False):
+    accuracy_arr = []
+    accuracy_shuff_arr = []
+    for each_seed in accuracies_dict.keys():
+        if pick_one:
+            accuracy_arr.append(accuracies_dict[each_seed][0])
+            accuracy_shuff_arr.append(accuracies_shuff_dict[each_seed][0])
+        else:
+            accuracy_arr.append(accuracies_dict[each_seed])
+            accuracy_shuff_arr.append(accuracies_shuff_dict[each_seed])
+    accuracy_arr = np.vstack(accuracy_arr)
+    accuracy_shuff_arr = np.vstack(accuracy_shuff_arr)
+    plt.figure()
+    plt.plot(np.arange(40), np.mean(accuracy_arr, axis=0), color='k', label='Non-shuffled')
+    plt.fill_between(np.arange(40), np.mean(accuracy_arr, axis=0)-np.std(accuracy_arr, axis=0), np.mean(accuracy_arr, axis=0)+np.std(accuracy_arr, axis=0), color='k', alpha=0.3)
+    plt.plot(np.arange(40), np.mean(accuracy_shuff_arr, axis=0), color='r', label='Unit-shuffled')
+    plt.fill_between(np.arange(40), np.mean(accuracy_shuff_arr, axis=0)-np.std(accuracy_shuff_arr, axis=0), np.mean(accuracy_shuff_arr, axis=0)+np.std(accuracy_shuff_arr, axis=0), color='r', alpha=0.3)
+    plt.xlabel('Time since delay onset')
+    plt.ylabel('Stimulus decoding accuracy')
+    plt.legend()
+    plt.savefig(os.path.join(save_dir, f"stim_decoding_accuracy{'_one_fold' if pick_one else ''}.svg"))
+    plt.savefig(os.path.join(save_dir, f"stim_decoding_accuracy{'_one_fold' if pick_one else ''}.png"))
+
+
+def plot_stl_mutual_information_across_seeds(stl_mutual_info_dict):
+    mutual_info_arr = []
+    for each_seed in stl_mutual_info_dict.keys():
+        if stl_mutual_info_dict[each_seed].shape[-1] != 4:
+            continue
+        mutual_info_arr.append(stl_mutual_info_dict[each_seed])
+    mutual_info_arr = np.vstack(mutual_info_arr)
+    n_total_neurons = mutual_info_arr.shape[0]
+    unrmd, timermd, stimrmd, locrmd = mutual_info_arr[:,0], mutual_info_arr[:,1], mutual_info_arr[:,2], mutual_info_arr[:,3]
+    stats = cbook.boxplot_stats(mutual_info_arr, labels=['Unrmd', 'Timermd', 'Stimrmd', 'Locrmd'], bootstrap=10000)
+    for i in range(len(stats)):
+        stats[i]['whislo'] = np.min(mutual_info_arr[:,i], axis=0)
+        stats[i]['whishi'] = np.max(mutual_info_arr[:,i], axis=0)
+    fig, axs = plt.subplots(1,1)
+    fig.suptitle(f'Mutual Information pooled across {len(stl_mutual_info_dict)} seeds')
+    # for i in range(n_total_neurons):
+    #     plt.plot([1, 2, 3], info_arr[i,:], color="gray", lw=1)
+    props = dict(color='indigo', linewidth=1.5)
+    axs.bxp(stats, showfliers=False, boxprops=props,
+            capprops=props, whiskerprops=props, medianprops=props)
+    plt.ylabel("Mutual information (bits)", fontsize=19)
+    #Run nonparametric test, and print p-values
+    print(f"total neurons: {n_total_neurons}")
+    from scipy.stats import kruskal
+    print("Kruskal-Wallis test p-values:")
+    print("Unrmd vs Timermd: ", kruskal(unrmd, timermd)[1])
+    print("Unrmd vs Stimrmd: ", kruskal(unrmd, stimrmd)[1])
+    print("Unrmd vs Locrmd: ", kruskal(unrmd, locrmd)[1])
+    print("Timermd vs Stimrmd: ", kruskal(timermd, stimrmd)[1])
+    print("Timermd vs Locrmd: ", kruskal(timermd, locrmd)[1])
+    print("Stimrmd vs Locrmd: ", kruskal(stimrmd, locrmd)[1])
+    plt.savefig(os.path.join(save_dir, f'STL_joint_encoding_info_pooled_{len(stl_mutual_info_dict)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.svg'))
+    plt.savefig(os.path.join(save_dir, f'STL_joint_encoding_info_pooled_{len(stl_mutual_info_dict)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.png'))
+
+
+def plot_sl_mutual_information_across_seeds(sl_mutual_info_dict):
+    mutual_info_arr = []
+    for each_seed in sl_mutual_info_dict.keys():
+        info_row = np.zeros(3)
+        I = sl_mutual_info_dict[each_seed]
+        unrmd, posrmd, timermd = I['I_unsfl_unrmd'], I['I_unsfl_posrmd'], I['I_unsfl_timermd']
+        info_row[0] = unrmd
+        info_row[1] = posrmd
+        info_row[2] = timermd
+        mutual_info_arr.append(info_row)
+    mutual_info_arr = np.vstack(mutual_info_arr)
+    n_total_neurons = mutual_info_arr.shape[0]
+    print(f"total neurons: {n_total_neurons}")
+    unrmd, posrmd, timermd = mutual_info_arr[:,0], mutual_info_arr[:,1], mutual_info_arr[:,2]
+    stats = cbook.boxplot_stats(mutual_info_arr, labels=['Unrmd', 'Posrmd', 'Timermd'], bootstrap=10000)
+    from scipy.stats import kruskal
+    print("Kruskal-Wallis test p-values:")
+    print("Unrmd vs. Posrmd: ", kruskal(unrmd, posrmd)[1])
+    print("Unrmd vs. Timermd: ", kruskal(unrmd, timermd)[1])
+    print("Posrmd vs. Timermd: ", kruskal(posrmd, timermd)[1])
+
+    for i in range(len(stats)):
+        stats[i]['whislo'] = np.min(mutual_info_arr[:,i], axis=0)
+        stats[i]['whishi'] = np.max(mutual_info_arr[:,i], axis=0)
+    fig, axs = plt.subplots(1,1)
+    fig.suptitle(f'Mutual Information pooled across {len(sl_mutual_info_dict)} seeds')
+    props = dict(color='indigo', linewidth=1.5)
+    axs.bxp(stats, showfliers=False, boxprops=props,
+            capprops=props, whiskerprops=props, medianprops=props)
+    plt.ylabel("Mutual information", fontsize=19)
+    plt.savefig(os.path.join(save_dir, f'SL_joint_encoding_info_pooled_{len(stl_mutual_info_dict)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.svg'))
+    plt.savefig(os.path.join(save_dir, f'SL_joint_encoding_info_pooled_{len(stl_mutual_info_dict)}seeds_{n_total_episodes}_{n_shuffle}_{percentile}.png'))
+
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--untrained', type=bool, default=False, help='whether to use untrained model')
+parser.add_argument('--nomem', type=bool, default=False, help='whether env was NoMem')
 args = parser.parse_args()
 untrained = args.untrained
+nomem = args.nomem
 
-data_dir = '/network/scratch/l/lindongy/timecell/data_collecting/tunl2d/mem_40_lstm_256_5e-06'
+if nomem:
+    data_dir = '/network/scratch/l/lindongy/timecell/data_collecting/tunl2d/nomem_40_lstm_256_5e-06'
+else:
+    data_dir = '/network/scratch/l/lindongy/timecell/data_collecting/tunl2d/mem_40_lstm_256_5e-06'
 seed_list = []
 for file in os.listdir(data_dir):
-    if re.match(f'mem_40_lstm_256_5e-06_seed_\d+_epi79999.pt_data.npz', file):
-        seed_list.append(int(file.split('_')[6]))
+    if nomem and not untrained:
+        if re.match(f'nomem_40_lstm_256_5e-06_seed_\d+_epi79999.pt_data.npz', file):
+            seed_list.append(int(file.split('_')[6]))
+    elif not nomem and not untrained:
+        if re.match(f'mem_40_lstm_256_5e-06_seed_\d+_epi79999.pt_data.npz', file):
+            seed_list.append(int(file.split('_')[6]))
+    else:  # untrained
+        if re.match(f'seed_\d+_untrained_agent_weight_frozen_data.npz', file):
+            seed_list.append(int(file.split('_')[1]))
 seed_list = sorted(seed_list)
 print(f"Number of good seeds: {len(seed_list)}; seed_list: {seed_list}")
+
+if nomem and untrained:
+    save_dir = '/network/scratch/l/lindongy/timecell/figures/fig_7/tunl2d_nomem_untrained_weight_frozen'
+elif nomem and not untrained:
+    save_dir = '/network/scratch/l/lindongy/timecell/figures/fig_7/tunl2d_nomem'
+elif not nomem and untrained:
+    save_dir = '/network/scratch/l/lindongy/timecell/figures/fig_7/tunl2d_untrained_weight_frozen'
+else:  # Mem and trained
+    save_dir = '/network/scratch/l/lindongy/timecell/figures/fig_7/tunl2d'
+
+print(f"Saving to {save_dir}")
+n_total_episodes = 5000
+len_delay = 40
+n_neurons = 256
+n_shuffle = 100
+percentile = 99
+
+ramping_cell_ids = {}
+time_cell_ids = {}
+t_test_dict = {}
+t_test_pred_dict = {}
+r_dict = {}
+accuracies_dict = {}
+accuracies_shuff_dict = {}
+splitter_cell_nums_ANOVA = []
+splitter_cell_nums_dis = []
+stl_mutual_info_dict = {}
+sl_mutual_info_dict = {}
+
+for i_seed, each_seed in enumerate(seed_list):
+    print(f'====================== Analyzing seed {each_seed} ...======================================')
+    seed_save_dir = os.path.join(save_dir, f'seed_{each_seed}')
+    # load data
+    if os.path.exists(os.path.join(seed_save_dir, 'ramping_cell_ids_seed.npy')) and \
+            os.path.exists(os.path.join(seed_save_dir, 'time_cell_ids_seed.npy')) and \
+            os.path.exists(os.path.join(seed_save_dir, 'splitter_cell_ids_ANOVA.npy')) and \
+            os.path.exists(os.path.join(seed_save_dir, 'splitter_cell_ids_dis.npy')) and \
+            os.path.exists(os.path.join(seed_save_dir, 't_test_seed.npy')) and \
+            os.path.exists(os.path.join(seed_save_dir,  't_test_pred_seed.npy')) and \
+            os.path.exists(os.path.join(seed_save_dir, 'accuracies.npy')) and \
+            os.path.exists(os.path.join(seed_save_dir, 'accuracies_shuff.npy')) and \
+            os.path.exists(os.path.join(seed_save_dir, 'stl_mutual_info.npy')) and \
+            os.path.exists(os.path.join(seed_save_dir, 'joint_encoding.npz')) and \
+            os.path.exists(os.path.join(seed_save_dir, 'r_arr.npy')):
+        print("Loading existing data...")
+        ramping_cell_ids[each_seed] = np.load(os.path.join(seed_save_dir, 'ramping_cell_ids_seed.npy')).item()
+        time_cell_ids[each_seed] = np.load(os.path.join(seed_save_dir, 'time_cell_ids_seed.npy')).item()
+        splitter_cell_nums_ANOVA.append(len(np.load(os.path.join(seed_save_dir, 'splitter_cell_ids_ANOVA.npy'), allow_pickle=True)))
+        splitter_cell_nums_dis.append(len(np.load(os.path.join(seed_save_dir, 'splitter_cell_ids_dis.npy'), allow_pickle=True)))
+        t_test_dict[each_seed] = np.load(os.path.join(seed_save_dir, 't_test_seed.npy'), allow_pickle=True)
+        t_test_pred_dict[each_seed] = np.load(os.path.join(seed_save_dir, 't_test_pred_seed.npy'), allow_pickle=True)
+        accuracies_dict[each_seed] = np.load(os.path.join(seed_save_dir, 'accuracies.npy'), allow_pickle=True)
+        accuracies_shuff_dict[each_seed] = np.load(os.path.join(seed_save_dir, 'accuracies_shuff.npy'), allow_pickle=True)
+        stl_mutual_info_dict[each_seed] = np.load(os.path.join(seed_save_dir, 'stl_mutual_info.npy'), allow_pickle=True)
+        r_dict[each_seed] = np.load(os.path.join(seed_save_dir, 'r_arr.npy'))
+        sl_mutual_info_dict[each_seed] = np.load(os.path.join(seed_save_dir, 'joint_encoding.npz'), allow_pickle=True)
+
+
+plot_count_time_and_ramping_cells(time_cell_ids, ramping_cell_ids)
+
+plot_r_across_seeds(r_dict)
+
+plot_stim_decoding_accuracy_across_seeds(accuracies_dict, accuracies_shuff_dict, pick_one=True)
+plot_stim_decoding_accuracy_across_seeds(accuracies_dict, accuracies_shuff_dict, pick_one=False)
+
+plot_stl_mutual_information_across_seeds(stl_mutual_info_dict)
+
+plot_sl_mutual_information_across_seeds(sl_mutual_info_dict)
+
+plot_time_decoding_across_seeds(t_test_dict, t_test_pred_dict, len_delay=len_delay)
+
+#plot_ST_mutual_information_across_seeds(info_dict)  # To-run after saving info_dict
+#plot_trajectory_decoding_across_seeds(trajectory_decoding_accuracy_dict)
+print(f"Average number of splitter cells (ANOVA): {np.mean(splitter_cell_nums_ANOVA)}, std: {np.std(splitter_cell_nums_ANOVA)}")
+print(f"Average number of splitter cells (dis): {np.mean(splitter_cell_nums_dis)}, std: {np.std(splitter_cell_nums_dis)}")
+
+print("Done!")
