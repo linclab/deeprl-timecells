99d47366044c295cb2cd209774dc3ff8d4450665
lindongy <lindongy@login-4.server.mila.quebec>
2023-08-17 21:15:27 -0400

    transfer learning

--- analysis/fig_1_plot_performance.py
+++ analysis/fig_1_plot_performance.py
@@ -7,31 +7,60 @@ sys.path.insert(1,'/home/mila/l/lindongy/deeprl-timecells')
 from analysis import utils_linclab_plot
 utils_linclab_plot.linclab_plt_defaults(font="Arial", fontdir="analysis/fonts")
 
+# DNMS to DDC
+# Define helper functions
+def bin_rewards(epi_rewards, window_size):
+    """
+    Average the epi_rewards with a moving window.
+    """
+    epi_rewards = epi_rewards.astype(np.float32)
+    avg_rewards = np.zeros_like(epi_rewards)
+    for i_episode in range(1, len(epi_rewards)+1):
+        if 1 < i_episode < window_size:
+            avg_rewards[i_episode-1] = np.mean(epi_rewards[:i_episode])
+        elif window_size <= i_episode <= len(epi_rewards):
+            avg_rewards[i_episode-1] = np.mean(epi_rewards[i_episode - window_size: i_episode])
+    return avg_rewards
 
 # ====== extract behavioural data and plot performance =====
 # load data
-data_dir = '/network/scratch/l/lindongy/timecell/training/tunl1d_og/nomem_40_lstm_128_5e-05'
-seed_list = []
+data_dir = '/network/scratch/l/lindongy/timecell/training/timing/tunl1d_pretrained/lstm_128_1e-05'
+# look for folders that match "model_$model'
+performance_by_model = {}
+seed_by_model = {}
 for file in os.listdir(data_dir):
-    if re.match(f'seed_\d+_total_200000episodes_performance_data.npz', file):
-        seed_list.append(int(file.split('_')[1]))
-performance_array = np.zeros((len(seed_list), 200000))
-for i, seed in enumerate(seed_list):
-    data = np.load(os.path.join(data_dir, f'seed_{seed}_total_200000episodes_performance_data.npz'), allow_pickle=True)
-    performance_array[i,:] = data['nomem_perf']
+    if re.match(f'model_\d+', file):
+        performance_by_model[(file.split('_')[1])] = []
+        seed_by_model[(file.split('_')[1])] = []
+for model in performance_by_model.keys():
+    print(f'Processing model {model}')
+    subdir = os.path.join(data_dir, f'model_{model}')
+    for file in os.listdir(subdir):
+        if re.match(f'seed_\d+_total_150000episodes_performance_data.npz', file):
+            seed_by_model[model].append(int(file.split('_')[1]))
+            # stim = np.load(os.path.join(subdir, file), allow_pickle=True)['stim']
+            # first_action = np.load(os.path.join(subdir, file), allow_pickle=True)['first_action']
+            # performance array the same shape as stim, where stim+first_action is 1
+            # performance_by_model[model].append((stim+first_action==1).astype(int))
+            performance_by_model[model].append(np.load(os.path.join(subdir, file), allow_pickle=True)['correct_trial'])
+    performance_by_model[model] = np.array(performance_by_model[model])
+breakpoint()
 
-
-# plot performance with mean and std
-mean_perf = np.mean(performance_array, axis=0)
-std_perf = np.std(performance_array, axis=0)
-
+# plot average and stf performance of each model
 plt.figure(figsize=(8,6))
-plt.plot(mean_perf, label='128-unit LSTM')
-plt.fill_between(np.arange(200000), mean_perf-std_perf, mean_perf+std_perf, alpha=0.2)
+for model in performance_by_model.keys():
+    mean_perf = bin_rewards(np.mean(performance_by_model[model], axis=0), window_size=1000)
+    std_perf = bin_rewards(np.std(performance_by_model[model], axis=0), window_size=1000)
+    plt.plot(mean_perf, label=f'Model {model} for DNMS')
+    plt.fill_between(np.arange(150000), mean_perf-std_perf, mean_perf+std_perf, alpha=0.1)
 plt.xlabel('Episode')
 plt.ylabel('% Correct')
 plt.ylim([0,1])
 plt.legend()
-plt.title("NoMem DNMS")
-plt.savefig('/network/scratch/l/lindongy/timecell/figures/tunl1d_nomem_performance.svg')
+plt.title("DNMS to DDC")
+plt.savefig('/network/scratch/l/lindongy/timecell/figures/tunl1d_to_ddc_performance_.svg')
+plt.savefig('/network/scratch/l/lindongy/timecell/figures/tunl1d_to_ddc_performance_.png')
+
+
+
 
---
+++ sbatch_sh_files/run_transfer_learning.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --array=1-50
+#SBATCH --job-name=ddc_to_dnms_data
+#SBATCH --output=/network/scratch/l/lindongy/timecell/sbatch_out/tunl1d_og/timing_pretrained_data-%A.%a.out
+#SBATCH --error=/network/scratch/l/lindongy/timecell/sbatch_err/tunl1d_og/timing_pretrained_data-%A.%a.err
+#SBATCH --partition=long
+#SBATCH --time=0:15:00
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20G
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=lindongy@mila.quebec
+
+module load python/3.7 python/3.7/cuda/11.0/cudnn/8.0/pytorch/1.7.0
+source $HOME/a100_env/bin/activate
+
+#model_seed_list=(104 106 109 110 114) #102 too  # DNMS dim-2, 6 out of 49 good seeds
+model_seed_list=(107 125 124 101 135) #102 too  # DDC 9 out of 9 good seeds
+task_seeds=(1 2 3 4 5 6 7 8 9 10)
+
+model_seed_idx=$((($SLURM_ARRAY_TASK_ID - 1) / 10))
+task_seed_idx=$((($SLURM_ARRAY_TASK_ID - 1) % 10))
+
+model_seed=${model_seed_list[$model_seed_idx]}
+task_seed=${task_seeds[$task_seed_idx]}
+
+#load_model_path="mem_40_lstm_128_5e-05/seed_${model_seed}_epi199999.pt"  # DNMS dim-2 models
+#load_model_path="lstm_128_1e-05/seed_${model_seed}_epi149999.pt"  # DDC models
+
+#load_model_path="model_${model_seed}/seed_${task_seed}_epi149999.pt" # collect data on DNMS->DDC model
+load_model_path="model_${model_seed}/seed_${task_seed}_epi199999.pt" # collect data on DDC-> DNMS model
+
+#python expts/run_int_discrim_1d.py --model_seed $model_seed --n_total_episodes 5000 --save_ckpt_per_episodes 2500 --load_model_path $load_model_path --n_neurons 128 --lr 0.00001 --seed $task_seed --hidden_type 'lstm' --record_data True
+
+python expts/run_tunl_1d.py --model_seed $model_seed --n_total_episodes 5000 --save_ckpt_per_episodes 2500 --load_model_path $load_model_path --n_neurons 128 --len_delay 40 --lr 0.00005 --seed $task_seed --env_type 'mem' --hidden_type 'lstm'
+
+
+
+
+#echo "Training DNMS model with model seed: $model_seed and DDC task seed: $task_seed"
+#echo "Training DDC model with model seed: $model_seed and DMNS task seed: $task_seed"
+#echo "Training DDC model with model seed: $model_seed and DNMS NoMem task seed: $task_seed"
+
+#python expts/run_int_discrim_1d.py --n_total_episodes 150000 --save_ckpt_per_episodes 50000 --load_model_path $load_model_path --save_ckpts True --n_neurons 128 --lr 0.00001 --seed $task_seed --hidden_type 'lstm' --save_performance_fig True
+#python expts/run_tunl_1d.py --n_total_episodes 200000 --save_ckpt_per_episodes 40000 --save_ckpts True --load_model_path $load_model_path --n_neurons 128 --len_delay 40 --lr 0.00005 --seed $task_seed --env_type 'mem' --hidden_type 'lstm' --save_performance_fig True
+#python expts/run_tunl_1d_nomem.py --n_total_episodes 200000 --save_ckpt_per_episodes 40000 --save_ckpts True --load_model_path $load_model_path --n_neurons 128 --len_delay 40 --lr 0.00005 --seed $task_seed --env_type 'nomem' --hidden_type 'lstm' --save_performance_fig True
