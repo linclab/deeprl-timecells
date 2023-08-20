e72146c8907ef8b87a689c589307861622cd60bc
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2023-08-20 12:43:39 -0400

    no_reset plot perf

--- analysis/fig_1_plot_performance.py
+++ analysis/fig_1_plot_performance.py
@@ -10,7 +10,7 @@ utils_linclab_plot.linclab_plt_defaults(font="Arial", fontdir="analysis/fonts")
 
 # ====== extract behavioural data and plot performance =====
 # load data
-data_dir = '/network/scratch/l/lindongy/timecell/training/tunl1d_og/nomem_40_lstm_128_5e-05'
+data_dir = '/network/scratch/l/lindongy/timecell/training/tunl1d_og/no_reset/mem_40_lstm_128_0.0001'
 seed_list = []
 for file in os.listdir(data_dir):
     if re.match(f'seed_\d+_total_200000episodes_performance_data.npz', file):
@@ -18,7 +18,10 @@ for file in os.listdir(data_dir):
 performance_array = np.zeros((len(seed_list), 200000))
 for i, seed in enumerate(seed_list):
     data = np.load(os.path.join(data_dir, f'seed_{seed}_total_200000episodes_performance_data.npz'), allow_pickle=True)
-    performance_array[i,:] = data['nomem_perf']
+    stim = data['stim']
+    first_action = data['first_action']
+    performance_array[i,:] = stim+first_action==1
+    #performance_array[i,:] = data['correct_trial']
 
 
 # plot performance with mean and std
@@ -27,11 +30,12 @@ std_perf = np.std(performance_array, axis=0)
 
 plt.figure(figsize=(8,6))
 plt.plot(mean_perf, label='128-unit LSTM')
-plt.fill_between(np.arange(200000), mean_perf-std_perf, mean_perf+std_perf, alpha=0.2)
+plt.fill_between(np.arange(150000), mean_perf-std_perf, mean_perf+std_perf, alpha=0.2)
 plt.xlabel('Episode')
 plt.ylabel('% Correct')
 plt.ylim([0,1])
 plt.legend()
-plt.title("NoMem DNMS")
-plt.savefig('/network/scratch/l/lindongy/timecell/figures/tunl1d_nomem_performance.svg')
+plt.title("DDC no reset")
+plt.savefig('/network/scratch/l/lindongy/timecell/figures/timing_no_reset_performance.svg')
+plt.savefig('/network/scratch/l/lindongy/timecell/figures/timing_no_reset_performance.png')
 
