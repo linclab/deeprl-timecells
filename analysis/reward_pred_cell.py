fbcf3e90bc2cf467fdd116a53915fb686c393d38
Dongyan Lin <56082488+dongyanl1n@users.noreply.github.com>
2023-11-29 19:49:46 -0500

    analysis pipeline

---
+++ analysis/reward_pred_cell.py
@@ -0,0 +1,185 @@
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+from tqdm import tqdm
+import sys
+sys.path.insert(1,'/home/mila/l/lindongy/deeprl-timecells')
+from analysis import utils_linclab_plot
+utils_linclab_plot.linclab_plt_defaults(font="Arial", fontdir="analysis/fonts")
+from utils_incentive import *
+
+# argparse each seed
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--seed', type=int, default=0)
+parser.add_argument('--untrained', type=bool, default=False, help='whether to use untrained model')
+parser.add_argument('--env_type', type=str, default='mem', help='mem or nomem')
+args = parser.parse_args()
+each_seed = args.seed
+untrained = args.untrained
+env_type = args.env_type
+
+data_dir = f"/network/scratch/l/lindongy/timecell/data_collecting/tunl2d/{env_type}_40_lstm_256_5e-06_smallrwd0.5_4"
+n_total_episodes = 5000
+len_delay = 40
+n_neurons = 256
+n_shuffle = 100
+percentile = 99
+p_small_reward = 0.5
+a_small_reward = 3
+small_rwd = 100/a_small_reward
+
+
+if untrained:
+    save_dir = f'/network/scratch/l/lindongy/timecell/figures/tunl2d_incentive/{env_type}/untrained'
+else:
+    save_dir = f'/network/scratch/l/lindongy/timecell/figures/tunl2d_incentive/{env_type}/trained'
+os.makedirs(save_dir, exist_ok=True)
+
+
+print(f'====================== Analyzing seed {each_seed} ...======================================')
+seed_save_dir = os.path.join(save_dir, f'seed_{each_seed}')
+if not os.path.exists(seed_save_dir):
+    os.makedirs(seed_save_dir, exist_ok=True)
+# load the data
+print("load the data...")
+
+if untrained:
+    data = np.load(os.path.join(data_dir, f'seed_{each_seed}_untrained_agent_weight_frozen_data.npz'), allow_pickle=True)
+else:
+    data = np.load(os.path.join(data_dir, f'{env_type}_40_lstm_256_5e-06_smallrwd0.5_4_seed_{each_seed}_epi79999.pt_data.npz'), allow_pickle=True)
+
+stim = data['stim']  # (n_episodes, 2)
+choice = data['choice'] # (n_episodes, 2)
+ct = data['ct'] # (n_episodes)
+neural_activity = data['neural_activity']  # (n_episodes,) array. Each element of the array is a list of length T_episode. Each element of the list is a numpy array of shape (n_neurons,).
+action = data['action'] # (n_episodes,) array. Each element of the array is a list of length T_episode. Each element of the list is an integer.
+location = data['location'] # (n_episodes,) array. Each element of the array is a list of length T_episode. Each element of the list is a numpy array of shape (2,).
+reward = data['reward'] # (n_episodes, ) array. Each element of the array is a list of length T_episode. Each element of the list is a float.
+epi_small_reward = data['epi_small_reward'] # (n_episodes)  # whether agent receives small reward in this episode
+epi_to_incentivize = data['epi_to_incentivize'] # (n_episodes)  # whether agent should receive small reward in this episode
+
+# get timestamps
+print("get timestamps...")
+
+timestamp_initiate_stimulus = []
+timestamp_poke_stimulus = []
+timestamp_initiate_choice = []
+timestamp_poke_choice = []  # associated with reward 5 for correct trials or -20 for incorrect trials
+timestamp_small_reward = []
+timestamp_big_reward = []  # for correct trials only; collecting final big reward
+
+for i_episode in tqdm(range(n_total_episodes)):
+
+    action_episode = np.array(action[i_episode])  # T_episode
+    location_episode = np.array(location[i_episode])  # T_episode x 2
+    reward_episode = np.array(reward[i_episode])  # T_episode
+
+    # stimulus-initiation, sample-poke, choice-initiation
+    poke_reward_timestamp = np.where(np.asarray(reward_episode) == 5)[0]
+    if len(poke_reward_timestamp) == 4: # correct trial
+        # assert trials with 4 poke_reward_timestamps are correct trials, for mem or nomem
+        if env_type == 'mem':
+            assert np.any(stim[i_episode] != choice[i_episode])
+        elif env_type == 'nomem':
+            assert np.all(choice[i_episode] == [1,1])
+        assert  reward_episode[-1] == 100
+
+        stim_init_idx, sample_poke_idx, choice_init_idx, choice_poke_idx = poke_reward_timestamp
+        timestamp_big_reward.append(len(reward_episode) - 1)  # the last item
+    elif len(poke_reward_timestamp) == 3: # incorrect trial
+        # assert trials with 3 poke_reward_timestamps are incorrect trials, for mem or nomem
+        if env_type == 'mem':
+            assert np.all(stim[i_episode] == choice[i_episode])
+        elif env_type == 'nomem':
+            assert np.all(choice[i_episode] == [1,7])
+        assert reward_episode[-1] == -20
+
+        stim_init_idx, sample_poke_idx, choice_init_idx = poke_reward_timestamp
+        choice_poke_idx = len(reward_episode) - 1  # the last item
+    else:
+        raise ValueError("Incorrect number of timestamps for reward: either 3 for incorrect trials or 4 for correct trials.")
+    timestamp_initiate_stimulus.append(stim_init_idx)
+    timestamp_initiate_choice.append(choice_init_idx)
+    timestamp_poke_stimulus.append(sample_poke_idx)
+    timestamp_poke_choice.append(choice_poke_idx)
+    timestamp_small_reward.append(np.where(np.isclose(reward_episode, small_rwd))[0][0])
+
+
+timestamp_initiate_stimulus = np.asarray(timestamp_initiate_stimulus)
+timestamp_initiate_choice = np.asarray(timestamp_initiate_choice)
+timestamp_small_reward = np.asarray(timestamp_small_reward)
+timestamp_big_reward = np.asarray(timestamp_big_reward)
+timestamp_poke_stimulus = np.asarray(timestamp_poke_stimulus)
+timestamp_poke_choice = np.asarray(timestamp_poke_choice)
+
+trial_idx_initate_stimulus = trial_idx_intiate_choice = np.arange(5000)
+# get the indices of the episodes that received small reward
+trial_idx_small_reward = np.where(epi_small_reward != -3.9)[0]
+# get the indices of the episodes that should receive a small reward but did not receive one
+trial_idx_small_reward_missed = np.where((epi_small_reward == -3.9) & (epi_to_incentivize == 1))[0]
+# sum of len(idx_small_reward) and len(idx_small_reward_missed) should be ~p_small_reward*n_total_episodes
+
+# get correct-choice and incorrect-choice trial indices
+trial_idx_nonmatch = np.where(np.any(stim != choice, axis=1))[0]  # correct for mem
+trial_idx_match = np.where(np.all(stim == choice, axis=1))[0]  # incorrect for mem
+trial_idx_left_choice = np.where(np.all(choice==[1,1], axis=1))[0] # correct for nomem
+trial_idx_right_choice = np.where(np.all(choice==[1,7], axis=1))[0] # incorrect for nomem
+trial_idx_left_stimulus = np.where(np.all(stim==[1,1], axis=1))[0]
+trial_idx_right_stimulus = np.where(np.all(stim==[1,7], axis=1))[0]
+
+
+# identify neurons that fire significantly at different time steps
+print("identify neurons that fire significantly when collecting small reward...")
+small_reward_cells_idx = identify_significant_neurons(neural_activity, trial_idx=trial_idx_small_reward, timestamp=timestamp_small_reward, percentile=percentile)
+print(f"Number of neurons that fire significantly when collecting small reward: {len(small_reward_cells_idx)}")
+
+print("identify neurons that fire significantly when initiating stimulus phase...")
+initiate_stimulus_cells_idx = identify_significant_neurons(neural_activity, trial_idx=trial_idx_initate_stimulus, timestamp=timestamp_initiate_stimulus, percentile=percentile)
+print(f"Number of neurons that fire significantly when initiating stimulus phase: {len(initiate_stimulus_cells_idx)}")
+
+print("identify neurons that fire significantly when initiating choice phase...")
+initiate_choice_cells_idx = identify_significant_neurons(neural_activity, trial_idx=trial_idx_intiate_choice, timestamp=timestamp_initiate_choice, percentile=percentile)
+print(f"Number of neurons that fire significantly when initiating choice phase: {len(initiate_choice_cells_idx)}")
+
+print("identify neurons that fire significantly when collecting big reward...")
+big_reward_cells_idx = identify_significant_neurons(neural_activity, trial_idx=trial_idx_nonmatch if env_type=="mem" else trial_idx_left_choice, timestamp=timestamp_big_reward, percentile=percentile)
+print(f"Number of neurons that fire significantly when collecting big reward: {len(big_reward_cells_idx)}")
+
+print("identify neurons that fire significantly between poking stimulus and collecting incentive..")
+incentive_prediction_cells_idx = identify_significant_neurons_between_timestamps(neural_activity, trial_idx=trial_idx_small_reward, timestamp_start=timestamp_poke_stimulus, timestamp_end=timestamp_small_reward, percentile=percentile)
+print(f"Number of neurons that fire significantly between poking stimulus and collecting incentive: {len(incentive_prediction_cells_idx)}")
+
+print("identify neurons that fire significantly between poking choice and collecting big reward..")
+reward_prediction_cells_idx = identify_significant_neurons_between_timestamps(neural_activity, trial_idx=trial_idx_nonmatch if env_type=="mem" else trial_idx_left_choice, timestamp_start=timestamp_poke_choice, timestamp_end=timestamp_big_reward, percentile=percentile)
+print(f"Number of neurons that fire significantly between poking choice and collecting big reward: {len(reward_prediction_cells_idx)}")
+
+# for small_reward_cells, plot aggregated activity around small reward timestamp
+print("plot aggregated activity around small reward timestamp...")
+aggregated_activity = aggregate_neural_activity(neural_activity, trial_idx_small_reward, timestamp_small_reward, window_size=5, n_neurons=256)
+plot_aggregated_activity(aggregated_activity, small_reward_cells_idx, save_dir=os.path.join(save_dir, 'small_reward', 'first_100'), window_size=5, n_trials=100, random_trials=False)
+plot_aggregated_activity(aggregated_activity, small_reward_cells_idx, save_dir=os.path.join(save_dir, 'small_reward', 'random'), window_size=5, n_trials=100, random_trials=True)
+
+# for big_reward_cells, plot aggregated activity around big reward timestamp
+print("plot aggregated activity around big reward timestamp...")
+aggregated_activity = aggregate_neural_activity(neural_activity, trial_idx_nonmatch if env_type=="mem" else trial_idx_left_choice, timestamp_big_reward, window_size=5, n_neurons=256)
+plot_aggregated_activity(aggregated_activity, big_reward_cells_idx, save_dir=os.path.join(save_dir, 'big_reward', 'first_100'), window_size=5, n_trials=100, random_trials=False)
+plot_aggregated_activity(aggregated_activity, big_reward_cells_idx, save_dir=os.path.join(save_dir, 'big_reward', 'random'), window_size=5, n_trials=100, random_trials=True)
+
+# for incentive_prediction_cells, plot aggregated activity between poking stimulus and collecting incentive
+print("plot aggregated activity between poking stimulus and collecting incentive...")
+aggregated_activity = aggregate_run_to_reward_activity(neural_activity, trial_idx_small_reward, timestamp_poke_stimulus, timestamp_small_reward, n_neurons=256)
+plot_aggregated_activity(aggregated_activity, incentive_prediction_cells_idx, save_dir=os.path.join(save_dir, 'incentive_prediction', 'first_100'), window_size=5, n_trials=100, random_trials=False)
+plot_aggregated_activity(aggregated_activity, incentive_prediction_cells_idx, save_dir=os.path.join(save_dir, 'incentive_prediction', 'random'), window_size=5, n_trials=100, random_trials=True)
+
+# for reward_prediction_cells, plot aggregated activity between poking choice and collecting big reward
+print("plot aggregated activity between poking choice and collecting big reward...")
+aggregated_activity = aggregate_run_to_reward_activity(neural_activity, trial_idx_nonmatch if env_type=="mem" else trial_idx_left_choice, timestamp_poke_choice, timestamp_big_reward, n_neurons=256)
+plot_aggregated_activity(aggregated_activity, reward_prediction_cells_idx, save_dir=os.path.join(save_dir, 'reward_prediction', 'first_100'), window_size=5, n_trials=100, random_trials=False)
+plot_aggregated_activity(aggregated_activity, reward_prediction_cells_idx, save_dir=os.path.join(save_dir, 'reward_prediction', 'random'), window_size=5, n_trials=100, random_trials=True)
+
+
+
+
---
+++ analysis/utils_incentive.py
@@ -0,0 +1,202 @@
+import numpy as np
+from scipy import stats
+import itertools
+from utils_mutual_info import shuffle_activity
+import matplotlib.pyplot as plt
+import os
+from matplotlib.backends.backend_pdf import PdfPages
+from tqdm import tqdm
+import sklearn
+import scikit_posthocs as sp
+import utils_linclab_plot
+from utils_analysis import shuffle_activity, shuffle_activity_single_neuron, shuffle_activity_single_neuron_varying_duration
+import sys
+sys.path.insert(1,'/home/mila/l/lindongy/deeprl-timecells')
+from analysis import utils_linclab_plot
+utils_linclab_plot.linclab_plt_defaults(font="Arial", fontdir="analysis/fonts")
+
+#===================================================================================================
+# trial_neural_activity = neural_activity[i_episode].T  # n_neurons x T_episode
+def circular_shuffle_neural_activity(trial_neural_activity):
+    # trial_neural_activity: n_neurons x T_episode
+    # circularly shift bins for each neuron within a trial (i.e., row-wise), thus destroying
+    # pairwise correlations but preserving single-cell autocorrelations.
+    # return: n_neurons x T_episode
+    n_neurons = trial_neural_activity.shape[0]
+    T_episode = trial_neural_activity.shape[1]
+    trial_neural_activity_shuffled = np.zeros((n_neurons, T_episode))
+    for i_neuron in range(n_neurons):
+        trial_neural_activity_shuffled[i_neuron] = np.roll(trial_neural_activity[i_neuron], np.random.randint(T_episode))
+    return trial_neural_activity_shuffled  # n_neurons x T_episode
+
+def swap_shuffle_neural_activity(trial_neural_activity):
+    # trial_neural_activity: n_neurons x T_episode
+    # randomly permute columns of the trial_neural_activity,
+    # thus preserving instantaneous pairwise correlations but destroying autocorrelations
+    # return: n_neurons x T_episode
+    n_neurons = trial_neural_activity.shape[0]
+    T_episode = trial_neural_activity.shape[1]
+    trial_neural_activity_shuffled = np.zeros((n_neurons, T_episode))
+    for i_neuron in range(n_neurons):
+        trial_neural_activity_shuffled[i_neuron] = trial_neural_activity[i_neuron][np.random.permutation(T_episode)]
+    return trial_neural_activity_shuffled
+
+#===================================================================================================
+
+# identify if neuron's average activity at timestamp across trials is significantly (>99 percentile) higher than circularly shuffled average activity of this neuron at this timestamp across trials
+def get_trial_average_value_at_time_step(neural_activity, trial_idx, timestamp, n_neurons=256):
+    n_trials = len(trial_idx)
+    assert n_trials == len(timestamp)
+    trial_average_value = np.zeros(n_neurons)
+    # neural_activity: n_episodes, each element is a list of length T_episode, each element is a numpy array of shape (n_neurons,)
+    for i_neuron in range(n_neurons):
+        trial_average_value[i_neuron] = np.mean([neural_activity[trial_idx[i_trial]][timestamp[i_trial]][i_neuron] for i_trial in range(n_trials)])
+    return trial_average_value  # n_neurons
+
+
+def get_trial_average_value_within_window(neural_activity, trial_idx, timestamp, window_size=5, n_neurons=256):
+    # get trial average value within window_size bins of timestamp
+    n_trials = len(trial_idx)
+    assert n_trials == len(timestamp)
+    trial_average_value = np.zeros(n_neurons)
+    # neural_activity: n_episodes, each element is a list of length T_episode, each element is a numpy array of shape (n_neurons,)
+    for i_neuron in range(n_neurons):
+        trial_average_value[i_neuron] = np.mean([np.mean(neural_activity[trial_idx[i_trial]][timestamp[i_trial]-window_size:timestamp[i_trial]+window_size+1][i_neuron]) for i_trial in range(n_trials)])
+    return trial_average_value  # n_neurons
+
+def get_trial_average_value_between_timestamps(neural_activity, trial_idx, timestamp_start, timestamp_end, n_neurons=256):
+# get trial average value between timestamp_start and timestamp_end
+    n_trials = len(trial_idx)
+    assert n_trials == len(timestamp_start) == len(timestamp_end)
+    trial_average_value = np.zeros(n_neurons)
+    # neural_activity: n_episodes, each element is a list of length T_episode, each element is a numpy array of shape (n_neurons,)
+    for i_neuron in range(n_neurons):
+        trial_average_value[i_neuron] = np.mean([np.mean(neural_activity[trial_idx[i_trial]][timestamp_start[i_trial]:timestamp_end[i_trial]][i_neuron]) for i_trial in range(n_trials)])
+    return trial_average_value  # n_neurons
+
+def get_trial_average_value_at_time_step_shuffled(neural_activity, trial_idx, timestamp, n_neurons=256, shuffle='circular'):
+    # use circular shuffle or swap shuffle to shuffle neural activity
+    n_trials = len(trial_idx)
+    assert n_trials == len(timestamp)
+    trial_average_value_shuffled = np.zeros(n_neurons)
+    # neural_activity: n_episodes, each element is a list of length T_episode, each element is a numpy array of shape (n_neurons,)
+    for i_neuron in range(n_neurons):
+        if shuffle == 'circular':
+            trial_average_value_shuffled[i_neuron] = np.mean([circular_shuffle_neural_activity(neural_activity[trial_idx[i_trial]].T)[i_neuron][timestamp[i_trial]] for i_trial in range(n_trials)])
+        elif shuffle == 'swap':
+            trial_average_value_shuffled[i_neuron] = np.mean([swap_shuffle_neural_activity(neural_activity[trial_idx[i_trial]].T)[i_neuron][timestamp[i_trial]] for i_trial in range(n_trials)])
+    return trial_average_value_shuffled  # n_neurons
+
+def get_trial_average_value_within_window_shuffled(neural_activity, trial_idx, timestamp, window_size=5, n_neurons=256, shuffle='circular'):
+    # shuffle neural activity, then take average within window_size bins of timestamp
+    n_trials = len(trial_idx)
+    assert n_trials == len(timestamp)
+    trial_average_value_shuffled = np.zeros(n_neurons)
+    # neural_activity: n_episodes, each element is a list of length T_episode, each element is a numpy array of shape (n_neurons,)
+    for i_neuron in range(n_neurons):
+        if shuffle == 'circular':
+            trial_average_value_shuffled[i_neuron] = np.mean([np.mean(circular_shuffle_neural_activity(neural_activity[trial_idx[i_trial]].T)[i_neuron][timestamp[i_trial]-window_size:timestamp[i_trial]+window_size+1]) for i_trial in range(n_trials)])
+        elif shuffle == 'swap':
+            trial_average_value_shuffled[i_neuron] = np.mean([np.mean(swap_shuffle_neural_activity(neural_activity[trial_idx[i_trial]].T)[i_neuron][timestamp[i_trial]-window_size:timestamp[i_trial]+window_size+1]) for i_trial in range(n_trials)])
+
+def get_trial_average_value_between_timestamps_shuffled(neural_activity, trial_idx, timestamp_start, timestamp_end, n_neurons=256, shuffle='circular'):
+    # shuffle neural activity, then take average between timestamp_start and timestamp_end
+    n_trials = len(trial_idx)
+    assert n_trials == len(timestamp_start) == len(timestamp_end)
+    trial_average_value_shuffled = np.zeros(n_neurons)
+    # neural_activity: n_episodes, each element is a list of length T_episode, each element is a numpy array of shape (n_neurons,)
+    for i_neuron in range(n_neurons):
+        if shuffle == 'circular':
+            trial_average_value_shuffled[i_neuron] = np.mean([np.mean(circular_shuffle_neural_activity(neural_activity[trial_idx[i_trial]].T)[i_neuron][timestamp_start[i_trial]:timestamp_end[i_trial]]) for i_trial in range(n_trials)])
+        elif shuffle == 'swap':
+            trial_average_value_shuffled[i_neuron] = np.mean([np.mean(swap_shuffle_neural_activity(neural_activity[trial_idx[i_trial]].T)[i_neuron][timestamp_start[i_trial]:timestamp_end[i_trial]]) for i_trial in range(n_trials)])
+    return trial_average_value_shuffled  # n_neurons
+
+
+def identify_significant_neurons(neural_activity, trial_idx, timestamp, use_window_average=False, window_size=5, n_shuffle=1000, n_neurons=256, percentile=99, shuffle='circular'):
+    # identify if neuron's average activity at timestamp across trials is significantly (>99 percentile) higher than 1000 circularly shuffled average activities of this neuron at this timestamp across trials
+    if use_window_average:
+        trial_average_value = get_trial_average_value_within_window(neural_activity, trial_idx, timestamp, window_size=window_size, n_neurons=n_neurons)
+    else:
+        trial_average_value = get_trial_average_value_at_time_step(neural_activity, trial_idx, timestamp, n_neurons=n_neurons)
+    trial_average_value_shuffled = []
+    for i_shuffle in range(n_shuffle):
+        if use_window_average:
+            trial_average_value_shuffled.append(get_trial_average_value_within_window_shuffled(neural_activity, trial_idx, timestamp, window_size=window_size, n_neurons=n_neurons, shuffle=shuffle))
+        else:
+            trial_average_value_shuffled.append(get_trial_average_value_at_time_step_shuffled(neural_activity, trial_idx, timestamp, n_neurons=n_neurons, shuffle=shuffle))
+    return np.where(trial_average_value > np.percentile(trial_average_value_shuffled, percentile))[0] # indices of neurons that are significantly higher than shuffled neurons
+
+def identify_significant_neurons_between_timestamps(neural_activity, trial_idx, timestamp_start, timestamp_end, n_shuffle=1000, n_neurons=256, percentile=99, shuffle='circular'):
+    trial_average_value = get_trial_average_value_between_timestamps(neural_activity, trial_idx, timestamp_start, timestamp_end, n_neurons=n_neurons)
+    trial_average_value_shuffled = []
+    for i_shuffle in range(n_shuffle):
+        trial_average_value_shuffled.append(get_trial_average_value_between_timestamps_shuffled(neural_activity, trial_idx, timestamp_start, timestamp_end, n_neurons=n_neurons, shuffle=shuffle))
+    return np.where(trial_average_value > np.percentile(trial_average_value_shuffled, percentile))[0] # indices of neurons that are significantly higher than shuffled neurons
+
+
+
+
+# aggregate neural activity across trials for timestamp +/- 5 bins, given neuron number, trial indices, and timestamps
+# if timestamp +5 bins is out of length of trial, then fill with nan
+def aggregate_neural_activity(neural_activity, trial_idx, timestamp, window_size=5, n_neurons=256):
+    n_trials = len(trial_idx)
+    assert n_trials == len(timestamp)
+    aggregated_activity = np.zeros((n_trials, window_size*2+1, n_neurons))
+    for i_neuron in range(n_neurons):
+        agg_activity = np.empty((n_trials, window_size*2+1))
+        agg_activity[:] = np.nan
+        for i_trial in range(n_trials):
+            trial_neural_activity = neural_activity[trial_idx[i_trial]].T # n_neurons x T_episode
+            timestamp_trial = timestamp[i_trial]
+            if timestamp_trial - window_size < 0:  # eg. initate stimulus
+                agg_activity[i_trial][window_size-timestamp_trial:] = trial_neural_activity[i_neuron][:timestamp_trial+window_size+1]
+            elif timestamp_trial + window_size > len(trial_neural_activity[i_neuron]):  # eg. poke choice
+                agg_activity[i_trial][:len(trial_neural_activity[i_neuron])-timestamp_trial+window_size+1] = trial_neural_activity[i_neuron][timestamp_trial-window_size:]
+            else:
+                agg_activity[i_trial] = trial_neural_activity[i_neuron][timestamp_trial-window_size:timestamp_trial+window_size+1]
+        aggregated_activity[:,:,i_neuron] = agg_activity
+    return aggregated_activity  # n_trials x window_size*2+1 x n_neurons
+
+
+def aggregate_run_to_reward_activity(neural_activity, trial_idx, timestamp_start, timestamp_end, n_neurons=256):
+    # trial_idx is all 5000 for sample-smallreward run, or all trial_idx_nonmatch or trial_idx_left_choice for choice-bigreward run
+    n_trials = len(timestamp_start)
+    assert n_trials == len(timestamp_end) == len(trial_idx)
+    aggregated_activity = np.zeros((n_trials, max(timestamp_end-timestamp_start), n_neurons))
+    for i_neuron in range(n_neurons):
+        agg_activity = np.empty((n_trials, max(timestamp_end-timestamp_start)))
+        agg_activity[:] = np.nan
+        for i_trial in range(n_trials):
+            trial_neural_activity = neural_activity[trial_idx[i_trial]].T # n_neurons x T_episode
+            agg_activity[i_trial] = trial_neural_activity[i_neuron][timestamp_start[i_trial]:timestamp_end[i_trial]]
+        aggregated_activity[:,:,i_neuron] = agg_activity
+    return aggregated_activity  # n_trials x max(timestamp_end-timestamp_start) x n_neurons
+
+
+# Plot each neurons's aggregated activity
+def plot_aggregated_activity(aggregated_activity, neuron_idx, save_dir, window_size=5, n_trials=100, random_trials=False):
+    for i_neuron in neuron_idx: #eg. for identified reward cells, or sample cells
+        fig, ax = plt.subplots(figsize=(10, 5))
+        if len(aggregated_activity) < n_trials:
+            n_trials = len(aggregated_activity)
+            trials_to_plot = np.arange(n_trials)
+        else:
+            trial_idx = np.arange(len(aggregated_activity))
+            if random_trials:
+                trials_to_plot = np.sort(np.random.choice(trial_idx, size=n_trials, replace=False))
+            else:
+                trials_to_plot = trial_idx[:n_trials]  # plot the first n_trials trials
+        ax.imshow(aggregated_activity[trials_to_plot,:,i_neuron], cmap='viridis', aspect='auto',interpolation='nearest')
+        ax.set_title(f'Neuron {i_neuron}')
+        ax.set_xlabel('Time (bins)')
+        ax.set_ylabel('Trials')
+        ax.set_xticks(np.arange(0, window_size*2+1, 1))
+        ax.set_xticklabels(np.arange(-window_size, window_size+1, 1))
+        ax.set_yticks(np.arange(0, n_trials, 1))
+        ax.set_yticklabels(np.arange(1, n_trials+1, 1))
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir, exist_ok=True)
+        plt.savefig(os.path.join(save_dir, f'neuron_{i_neuron}.png'))
+        plt.close()
+
